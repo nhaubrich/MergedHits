92a25aa557d67fcc5ba1b4f4e0129a33ef01c626
Brandon Bonham <BrandonBonham@gmail.com>
2020-09-18 13:58:19 -0500

    Intermediate update...Not ready for use.

--- Classifier_Training.py
+++ Classifier_Training.py
@@ -7,43 +7,53 @@ import matplotlib
 matplotlib.use('Agg') # for use without ssh -Y
 import matplotlib.pyplot as plt
 
-### The main inputs you may want to vary are: filenumber, entries_stop, max_epochs.
 
-def GetData(filenumber=5, entries_stop=None, testTrainFrac=.5): # "entries_stop=None" to use all entries 
+# The main inputs you may want to vary
+fileNumber = 1
+entriesStop = 500 # "entries_stop=None" to use all entries
+maxEpochs=100
+earlyStoppingPatience = 5
+otherVariables = ["totalADCcount"]
+
+
+def GetData(testTrainFrac=.5):
     from keras.utils import to_categorical
+    global filename, signalstring
     
-    filenames = ["/uscms_data/d3/bbonham/TrackerProject/Output_of_Postprocess/SharedHits/NormalizedCharge/output_final.h5",
-                 "/uscms_data/d3/bbonham/TrackerProject/Output_of_Postprocess/SharedHits/AbsoluteCharge/output_final.h5",
-                 "/uscms_data/d3/bbonham/TrackerProject/Output_of_Postprocess/Shared100NonShared/NormalizedCharge/output_final.h5",
-                 "/uscms_data/d3/bbonham/TrackerProject/Output_of_Postprocess/Shared100NonShared/AbsoluteCharge/output_final.h5",
-                 "/uscms_data/d3/bbonham/TrackerProject/Output_of_Postprocess/AllHits/NormalizedCharge/output_final.h5",
-                 "/uscms_data/d3/bbonham/TrackerProject/Output_of_Postprocess/AllHits/AbsoluteCharge/output_final.h5"]
-    global filename
-    filename = filenames[filenumber]
-
-    # Signal Region if you choose a file with only shared hits
-    if filenumber == 0 or filenumber == 1:
+    filenames=["/uscms_data/d3/bbonham/TrackerProject/Output_of_Postprocess/SharedHits/NormalizedCharge/output_final.h5",
+               "/uscms_data/d3/bbonham/TrackerProject/Output_of_Postprocess/SharedHits/AbsoluteCharge/output_final.h5",
+               "/uscms_data/d3/bbonham/TrackerProject/Output_of_Postprocess/Shared100NonShared/NormalizedCharge/output_final.h5",
+               "/uscms_data/d3/bbonham/TrackerProject/Output_of_Postprocess/Shared100NonShared/AbsoluteCharge/output_final.h5",
+               "/uscms_data/d3/bbonham/TrackerProject/Output_of_Postprocess/AllHits/NormalizedCharge/output_final.h5",
+               "/uscms_data/d3/bbonham/TrackerProject/Output_of_Postprocess/AllHits/AbsoluteCharge/output_final.h5"]
+    filename = filenames[fileNumber]
+    
+    # Signal Region for files with only shared hits
+    if fileNumber in [0,1]:
         signalstring = "['nUniqueSimTracksInSharedHit']>1"
-    # Signal Region if you choose a file with shared and nonshared hits
-    if filenumber in [2,3,4,5]:
+    # Signal Region for files with shared and nonshared hits
+    if fileNumber in [2,3,4,5]:
         signalstring = "['isSharedHit']==1"
-        #signalstring = "['nUniqueSimTracksInSharedHit']>1"    
+        #signalstring = "['nUniqueSimTracksInSharedHit']>1"
     
-    df = pd.read_hdf(filename, key='df', mode='r', start=0, stop=entries_stop)
+    df = pd.read_hdf(filename, key='df', mode='r', start=0, stop=entriesStop)
     print '\nFile: '+filename
     print 'df size:',round(df.memory_usage().sum()/(1024.**2),1),"MB"
+    
+    # Scale down the totalADCcount
+    df[['totalADCcount']] = df[['totalADCcount']]/10000
    
     # Reset the indices in case rows have the same index (maybe caused by two-lambda events/vertices?)
     df = df.set_index(np.arange(df.shape[0]))
 
-    # (maybe in the future transition to saving df_train instead of consistent seeding)
     df_train,df_test = train_test_split(df, test_size=testTrainFrac, random_state=10)
 
     images_train = to_image(df_train)
     images_test = to_image(df_test)
 
-    otherVariables_train = df_train[["trackPt","trackEta"]]
-    otherVariables_test = df_test[["trackPt","trackEta"]]
+    num_otherVariables = len(otherVariables)
+    otherVariables_train = df_train[otherVariables]
+    otherVariables_test = df_test[otherVariables]
     
     train_data = [images_train, otherVariables_train]
     test_data = [images_test, otherVariables_test]
@@ -62,7 +72,7 @@ def DefineModel(size=20):
     from keras.layers import Input,Conv2D,Dense,concatenate,Flatten
     from keras.models import Model
     inputClusterImages = Input(shape=(size,size,1))
-    inputHitVariables = Input(shape=(2,))
+    inputHitVariables = Input(shape=(len(otherVariables),))
 
     #CNN on cluster images
     CNN = Conv2D(32, kernel_size=(8,8),padding='same',activation='relu')(inputClusterImages)
@@ -91,11 +101,11 @@ def to_image(df,size=20):
     return  np.expand_dims(np.expand_dims(df[pixels], axis=-1).reshape(-1,size,size), axis=-1)
     
 
-def TrainModel(classifier,data,labels,max_epochs=100,validation_split=0.1):
+def TrainModel(classifier,data,labels,validation_split=0.1):
     from keras.callbacks import EarlyStopping, ModelCheckpoint
-    early_stopping = EarlyStopping(monitor='val_loss', patience=10) # early epoch stopping
+    early_stopping = EarlyStopping(monitor='val_loss', patience=earlyStoppingPatience) # early epoch stopping
     
-    classifier.fit(data, labels, epochs=max_epochs, validation_split=validation_split, callbacks=[early_stopping])
+    classifier.fit(data, labels, epochs=maxEpochs, validation_split=validation_split, callbacks=[early_stopping])
     classifier.save('TrainedClassifier_'+filename[60:-16].replace('/','_')+'.h5')
     
     
@@ -112,14 +122,11 @@ def PlotROC(classifier,discriminants,labels):
     plt.plot(fpr_keras, tpr_keras, label='Keras (area = {:.3f})'.format(auc))
     plt.xlabel('False positive rate')
     plt.ylabel('True positive rate')
-    plt.title('ROC curve (area = {:.3f})'.format(auc))
-    plt.savefig("ROC.png")
+    plt.title('ROC Curve (Area = {:.3f})'.format(auc)+'\n'+filename[60:-16]+'\n'+str(otherVariables)+'\nSignal: '+signalstring)
+    plt.tight_layout()
+    plt.savefig('ROC_'+filename[60:-16].replace('/','_')+'.png')
     plt.close()
     
-def ROCArea(classifier,discriminants,labels):
-    fpr_keras, tpr_keras, thresholds_keras = roc_curve(labels[:,1], discriminants)
-    auc = np.trapz(tpr_keras,fpr_keras) 
-    print("ROC curve area: {:.3f}".format(auc))
 
 def PlotDiscriminants(train_probs,train_Y,test_probs,test_Y):
     train_signal_probs = train_probs[train_Y[:,0]==1]
@@ -135,7 +142,7 @@ def PlotDiscriminants(train_probs,train_Y,test_probs,test_Y):
     plt.legend(loc='best')
     plt.xlabel('Discriminant')
     plt.title('CNN Signal and Background Discriminants')
-    plt.savefig("disc.png")
+    plt.savefig('disc_'+filename[60:-16].replace('/','_')+'.png')
     plt.close()
 
 
@@ -145,8 +152,7 @@ def Run():
     TrainModel(model,train_X,train_Y)
     train_probs = EvaluateModel(model,train_X,train_Y)
     test_probs = EvaluateModel(model,test_X,test_Y)
-    ROCArea(model,test_probs,test_Y)
-    #PlotROC(model,test_probs,test_Y)
-    #PlotDiscriminants(train_probs,train_Y, test_probs,test_Y)
+    PlotROC(model,test_probs,test_Y)
+    PlotDiscriminants(train_probs,train_Y,test_probs,test_Y)
 
 Run()
